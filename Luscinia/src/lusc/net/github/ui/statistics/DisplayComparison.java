f5b66490c03fba058ff750620bac536f05acbbc8
Rob <rflachlan@gmail.com>
2015-03-23 23:45:03 +0000

    Additional Images and Distribution Options

---
+++ Luscinia/src/lusc/net/github/ui/statistics/DisplayComparison.java
@@ -0,0 +1,15 @@
+package lusc.net.github.ui.statistics;
+
+import javax.swing.JPanel;
+
+import lusc.net.github.analysis.AnalysisGroup;
+
+public class DisplayComparison extends JPanel{
+	
+	public DisplayComparison(AnalysisGroup ag, int[] pair, int dataType){
+		
+		
+		
+	}
+
+}
